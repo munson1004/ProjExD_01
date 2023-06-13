8c3bcc15431ecc8b5fa5a2aaad6afdd7e3b4d647
munson <c0a221368c@edu.teu.ac.jp>
2023-06-13 16:53:32 +0900

    ６問目

--- Documents/ProjExD2023/ex01/pygame_image.py
+++ Documents/ProjExD2023/ex01/pygame_image.py
@@ -12,15 +12,21 @@ def main():
     kk = pg.transform.rotozoom(kk_img, 10, 1.0)
     kkflip = [kk_img,kk]
     tmr = 0
+    x=0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
+        
+        screen.blit(bg_img, [0-x, 0])
+        screen.blit(bg_img, [1600-x, 0])
         screen.blit(kkflip[tmr%2],[300,200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        x += 1
+        if x>1600:
+            x=0
+        clock.tick(500)
+
 
 
 if __name__ == "__main__":
